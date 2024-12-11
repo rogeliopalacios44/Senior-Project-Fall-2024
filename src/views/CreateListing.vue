738a380edc4c0bbab368aa5c4214adf96f842256
Roy Palacios <punc4k3@MacBookAir.lan>
2024-12-10 21:52:49 -0600

    Updated create listing page as well as updated home page.

--- src/views/CreateListing.vue
+++ src/views/CreateListing.vue
@@ -24,7 +24,9 @@ const addData = async () => {
   const { data, error } = await supabase
     .from('books')
     .insert([
-      {book_name: newBook.value, ISBN_10: newISBN10.value, ISBN_13: newISBN13.value,author_names: newAuthor.value, book_price: newBookPrice.value, book_category: newBookCategory.value}
+      {book_name: newBook.value, ISBN_10: newISBN10.value, ISBN_13: newISBN13.value,author_names: newAuthor.value, book_price: newBookPrice.value, book_category: newBookCategory.value},
+      {book_name: newBook2.value, ISBN_10: newISBN10_2.value, ISBN_13: newISBN13_2.value,author_names: newAuthor2.value, book_price: newBookPrice2.value, book_category: newBookCategory2.value},
+      {book_name: newBook3.value, ISBN_10: newISBN10_3.value, ISBN_13: newISBN13_3.value,author_names: newAuthor3.value, book_price: newBookPrice3.value, book_category: newBookCategory3.value}
     ]);
 
   if (error) {
@@ -64,6 +66,73 @@ const addListingData = async () => {
     console.log('Data inserted:', data);
   }
 }
+
+const newBook2 = ref('')
+const newISBN10_2 = ref()
+const newISBN13_2 = ref('')
+const newBookCategory2 = ref('')
+const newAuthor2 = ref('')
+const newBookPrice2 = ref()
+const file2 = ref()
+const isVisible = ref(true);
+const isVisible2 = ref(true);
+
+const uploadImage2 = async () => {
+  if (file2.value) {
+    try {
+      const { data, error } = await supabase.storage
+        .from('book_pictures')
+        .upload('your-file-path/' + file2.value.name, file2.value);
+
+      if (error) {
+        console.error('Error uploading image:', error.message);
+      } else {
+        console.log('Image uploaded successfully:', data);
+      }
+    } catch (error) {
+      console.error('Error uploading image:', error.message);
+    }
+  }
+}
+const handleFileChange2 = (event) => {
+  file2.value = event.target.files[0];
+};
+
+const clearModels = () => {
+  return{
+    newBook2: ''
+  }
+  
+}
+
+const newBook3 = ref('')
+const newISBN10_3 = ref()
+const newISBN13_3 = ref('')
+const newBookCategory3 = ref('')
+const newAuthor3 = ref('')
+const newBookPrice3 = ref()
+const file3 = ref()
+
+const uploadImage3 = async () => {
+  if (file3.value) {
+    try {
+      const { data, error } = await supabase.storage
+        .from('book_pictures')
+        .upload('your-file-path/' + file3.value.name, file3.value);
+
+      if (error) {
+        console.error('Error uploading image:', error.message);
+      } else {
+        console.log('Image uploaded successfully:', data);
+      }
+    } catch (error) {
+      console.error('Error uploading image:', error.message);
+    }
+  }
+}
+const handleFileChange3 = (event) => {
+  file3.value = event.target.files[0];
+};
 </script>
 
 <template>
@@ -154,10 +223,111 @@ const addListingData = async () => {
         <!-- going to need to figure out how to display form boxes for book info again after add another book button is pressed 
              i was thinking of just using an if statement with the divs above to reshow the book info again at least 3 times, it 
              will look messy but thats one it can work -->
-        <button style="margin-top: 25px; margin-bottom: 50px; background-color: #f05023;">Add another book</button>
+        <button style="margin-top: 25px; margin-bottom: 50px; background-color: #f05023;" @click="isVisible = !isVisible; isVisible ? (newBook2 = '', newAuthor2 = '', newISBN10_2 = null, newISBN13_2 = '', newBookPrice2 = null, newBookCategory2 = '') : null">{{ isVisible ? 'Add another book' : 'Remove Book' }}</button>
+        <div v-show="!isVisible">
+
+          <!-- Book Title2 -->
+        <div class="form-group">
+          <label for="title">Book Title</label>
+          <input type="text" id="title" v-model="newBook2" placeholder="Enter book title" required />
+        </div>
+  
+        <!-- Author2 -->
+        <div class="form-group">
+          <label for="author">Author</label>
+          <input type="text" id="author" v-model="newAuthor2" placeholder="Enter author name" required />
+        </div>
+
+        <!-- ISBN 10 2 -->
+        <div class="form-group">
+          <label for="author">ISBN-10</label>
+          <input type="text" id="isbn10" v-model="newISBN10_2" placeholder="Enter ISBN 10 (optional)"/>
+        </div>
+
+        <!-- ISBN 13 2 -->
+        <div class="form-group">
+          <label for="author">ISBN-13</label>
+          <input type="text" id="isbn13" v-model="newISBN13_2" placeholder="Enter ISBN 13" required />
+        </div>
+  
+        <!-- Price2 -->
+        <div class="form-group">
+          <label for="price">Price</label>
+          <input type="number" id="price" v-model="newBookPrice2" placeholder="Enter price" min="0" required />
+        </div>
+  
+        <!-- Category 2-->
+        <div class="form-group">
+          <label for="category">Category</label>
+          <select v-model="newBookCategory2" required>
+            <option value="" disabled>Select category</option>
+            <option v-for="category in categories" :key="category" :value="category">
+              {{ category }}
+            </option>
+          </select>
+        </div>
+  
+        <!-- Image Upload2 -->
+        <div class="form-group">
+          <label for="image">Upload Book Image</label>
+          <input type="file" @change="handleFileChange2" />
+        </div>
+
+        <!-- Button2 -->
+        <button style="margin-top: 25px; margin-bottom: 50px; background-color: #f05023;" @click="isVisible2 = !isVisible2; isVisible2 ? (newBook3 = '', newAuthor3 = '', newISBN10_3 = null, newISBN13_3 = '', newBookPrice3 = null, newBookCategory3 = '') : null">{{ isVisible2 ? 'Add another book' : 'Remove Book' }}</button>
+        <div v-show="!isVisible2">
+
+          <!-- Book Title3 -->
+        <div class="form-group">
+          <label for="title">Book Title</label>
+          <input type="text" id="title" v-model="newBook3" placeholder="Enter book title" required />
+        </div>
+  
+        <!-- Author3 -->
+        <div class="form-group">
+          <label for="author">Author</label>
+          <input type="text" id="author" v-model="newAuthor3" placeholder="Enter author name" required />
+        </div>
+
+        <!-- ISBN 10 3 -->
+        <div class="form-group">
+          <label for="author">ISBN-10</label>
+          <input type="text" id="isbn10" v-model="newISBN10_3" placeholder="Enter ISBN 10 (optional)"/>
+        </div>
+
+        <!-- ISBN 13 3 -->
+        <div class="form-group">
+          <label for="author">ISBN-13</label>
+          <input type="text" id="isbn13" v-model="newISBN13_3" placeholder="Enter ISBN 13" required />
+        </div>
+  
+        <!-- Price3 -->
+        <div class="form-group">
+          <label for="price">Price</label>
+          <input type="number" id="price" v-model="newBookPrice3" placeholder="Enter price" min="0" required />
+        </div>
+  
+        <!-- Category3 -->
+        <div class="form-group">
+          <label for="category">Category</label>
+          <select v-model="newBookCategory3" required>
+            <option value="" disabled>Select category</option>
+            <option v-for="category in categories" :key="category" :value="category">
+              {{ category }}
+            </option>
+          </select>
+        </div>
+  
+        <!-- Image Upload3 -->
+        <div class="form-group">
+          <label for="image">Upload Book Image</label>
+          <input type="file" @change="handleFileChange3" />
+        </div>
+        </div>
+      </div>
   
         <!-- Submit Button -->
-        <button @click="addData(); uploadImage(); addListingData()">Submit</button>
+        <button @click="addData(); uploadImage(); addListingData(); uploadImage2(); uploadImage3();">Submit</button>
       </form>
     </div>
   
